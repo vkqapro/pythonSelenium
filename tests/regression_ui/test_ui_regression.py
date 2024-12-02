679f891aab4cc402887462502ee06dbb1eabfcfb
Vitaly Kamynin <vitalykamynin@Vitalys-MacBook-Pro.local>
2024-12-01 22:41:55 -0500

    1.6

--- tests/regression_ui/test_ui_regression.py
+++ tests/regression_ui/test_ui_regression.py
@@ -21,7 +21,7 @@ class TestUIRegression(BaseTest):
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-extensions")
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
         yield driver
         driver.quit()
